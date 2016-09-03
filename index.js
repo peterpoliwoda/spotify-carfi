691029bc8f9a0e5141daa846c21c370a054d8225
Peter Poliwoda <peterpoliwoda@ie.ibm.com>
2016-09-03 18:35:12 +0100

    api changes

--- index.js
+++ index.js
@@ -1,62 +1,64 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var carfiFile = require('./carfi.server.json');
 var fs = require('fs');
 // eslint-disable-next-line
 var colors = require('colors');
-
 var config = require('./config.json');
 
 var app = express();
 app.use(bodyParser.text({type: 'text/html'}));
 
-// GET download spotify songs
-app.get('/carfi', function(req, res) {
-    console.log(carfiFile);
-    res.json(carfiFile);
+// POST fake download spotify songs
+app.post('/carfi', function(req, res) {
+    var list =
+    'Ukeje - Film.mp3\n' +
+    'Vixen - Falala.mp3\n' +
+    'Weezer - Thank God For Girls.mp3';
+
+    console.log(list);
+    res.end(list);
 });
 
 var toDelete = function(contents) {
-    var playlistFiles = fs.readdirSync(config.musicFolderPath + '_server');
-    var deleteThose = [];
+    var playlistFiles = fs.readdirSync(config.musicFolderPath);
+    var deleteThose = '';
     for (var song in contents) {
         if (playlistFiles.indexOf(contents[song]) > -1 || contents[song] == '') {
             console.log(contents[song] + ' found in files.'.green);
         } else {
             console.log('Delete: '.red + contents[song]);
-            deleteThose.push(contents[song]);
+            deleteThose += contents[song] + '\n';
         }
     }
     return deleteThose;
 };
 
 var toDownload = function(contents) {
-    var playlistFiles = fs.readdirSync(config.musicFolderPath + '_server');
-    var downloadThose = [];
+    var playlistFiles = fs.readdirSync(config.musicFolderPath);
+    var downloadThose = '';
     for (var song in playlistFiles) {
         if (contents.indexOf(playlistFiles[song]) > -1) {
             console.log(playlistFiles[song] + ' already downloaded.'.grey);
         } else {
             console.log('Download: '.yellow + playlistFiles[song]);
-            downloadThose.push(playlistFiles[song]);
+            downloadThose += playlistFiles[song] + '\n';
         }
     }
     return downloadThose;
 };
 
-// POST carfi folder
-app.post('/carfi', function(req, res) {
+// POST carfi delete folder
+app.post('/carfi/delete', function(req, res) {
     var folderContents = req.body.split(';');
-    console.log(folderContents);
     var deleteThose = toDelete(folderContents);
-    var downloadThose = toDownload(folderContents);
+    res.end(deleteThose);
+});
 
-    var response = {
-        message: 'Request received',
-        delete: deleteThose,
-        download: downloadThose
-    };
-    res.json(response);
+// POST carfi delete folder
+app.post('/carfi/download', function(req, res) {
+    var folderContents = req.body.split(';');
+    var downloadThose = toDownload(folderContents);
+    res.end(downloadThose);
 });
 
 var server = app.listen(3000, function() {
